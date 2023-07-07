2ca2abf693df333b8a8f93b2144471744ac62310
Radoslaw Marcinowski <rm92.git@gmail.com>
2023-07-07 23:14:19 +0200

    Added MessageExtract() function

--- src/NMEA-0183.c
+++ src/NMEA-0183.c
@@ -5,4 +5,36 @@
 #include "NMEA-0183-types.h"
 
 
-GGA_Message_Data_Raw_t GGA_Message_Data_Raw = {.ID="GPGGA"};
+
+
+
+/** Extract GGA Message to GGA_Message_Data_Raw */
+static int GGA_Message_Extract(const char *MsgIn);
+
+
+
+/** Extract Message */
+static int Message_Extract(const char *MsgIn)
+{
+    char Msg_Data[20][20] = {0};
+    uint8_t i, k;
+    int8_t j;
+
+    if ('$' == *(MsgIn))
+    {
+        for(i=0, j=0, k=0; i<82; i++, j++)
+        {
+            if (',' == *(MsgIn + i))
+            {
+                k++;
+                j=-1;
+                continue;
+            }
+            Msg_Data[k][j] = *(MsgIn + i);
+        }
+    }
+    else
+    {
+        return -1;
+    }    
+}
